872d20165e380ed5a9fb9b452fc65b8f83d35646
ppolydoras <pavlos@raw-labs.com>
2024-11-28 13:43:53 +0200

    improved docs

--- oracle/query.sql
+++ oracle/query.sql
@@ -1,7 +1,7 @@
 -- This is a placeholder SQL query.
 -- You can replace this query with any valid SQL query against your Oracle database.
 -- For example:
--- SELECT * FROM your_database.your_table;
+-- SELECT * FROM oracle.<your_table>;
 -- If you introduce parameters, be sure to define them using RAW's parameter syntax.
 
 SELECT 'Hello, World!' AS greeting;
